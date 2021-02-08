25a202765973210741f022fae131751d934744f3
Tiago Marques <tiagobm564@gmail.com>
2021-02-07 23:17:10 -0300

    Added others course

---
+++ javascript/primeiro_passos_na_linguagem/1-my_fisrt_application.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+const age = 30;
+
+console.log(age);
+console.log(age+2);
+
+let nome = "Tiago";
+console.log(`My name is ${nome}`);
+
+nome = "Tiago Marques";
+console.log(`My name is ${nome}`);
